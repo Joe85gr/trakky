7b5d5727363e8bb90c30f488f7157ad350f2cffa
Joe85gr <info@rainingdreams.co.uk>
2024-09-25 08:53:40 +0100

    reverted button changes

--- trakky-client/src/components/ui/button.tsx
+++ trakky-client/src/components/ui/button.tsx
@@ -4,7 +4,7 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { twMerge } from 'tailwind-merge';
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center rounded-md text-xs md:text-sm font-thin md:font-light transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none ',
+  'inline-flex items-center justify-center rounded-md text-xs md:text-sm font-thin md:font-light transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
       variant: {
